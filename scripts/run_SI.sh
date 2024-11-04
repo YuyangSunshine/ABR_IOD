43ddf6a10e26c7b15755babe541e6ea317b74225
Yuyang <40997704+YuyangSunshine@users.noreply.github.com>
2024-11-04 16:53:43 +0800

    Update run_SI.sh
    Modify the mistake of '--feat ard'.

--- scripts/run_SI.sh
+++ scripts/run_SI.sh
@@ -12,28 +12,28 @@ step=1
 
 #task=19-1
 #name=ABR_LR001_BS4_ALPHA1_BETA1_GAMMA5
-#exp -t ${task} -n ${name} -s ${step} --feat afd -gamma 5.0 --uce --dist_type id -alpha 1.0 -beta 1.0 -mb 2000 -mt mean -cvd 0,1
+#exp -t ${task} -n ${name} -s ${step} --feat ard -gamma 5.0 --uce --dist_type id -alpha 1.0 -beta 1.0 -mb 2000 -mt mean -cvd 0,1
 #name=Finetune
 #exp -t ${task} -n ${name} -s ${step} -cvd 0,1
 
 
 task=15-5
 name=ABR_LR001_BS4_ALPHA05_BETA1_GAMMA1
-exp -t ${task} -n ${name} -s ${step} --feat afd -gamma 1.0 --uce --dist_type id -alpha 0.5 -beta 1.0 -mb 2000 -mt mean -cvd 0,1
+exp -t ${task} -n ${name} -s ${step} --feat ard -gamma 1.0 --uce --dist_type id -alpha 0.5 -beta 1.0 -mb 2000 -mt mean -cvd 0,1
 #name=Finetune
 #exp -t ${task} -n ${name} -s ${step} -cvd 0,1
 
 
 #task=10-10
 #name=ABR_LR005_BS4_ALPHA01_BETA05_GAMMA1
-#exp -t ${task} -n ${name} -s ${step} --feat afd -gamma 1.0 --uce --dist_type id -alpha 0.1 -beta 0.5 -mb 2000 -mt mean -cvd 0,1
+#exp -t ${task} -n ${name} -s ${step} --feat ard -gamma 1.0 --uce --dist_type id -alpha 0.1 -beta 0.5 -mb 2000 -mt mean -cvd 0,1
 #name=Finetune
 #exp -t ${task} -n ${name} -s ${step} -cvd 0,1
 
 
 #task=5-15
 #name=ABR_LR005_BS4_ALPHA01_BETA05_GAMMA1
-#exp -t ${task} -n ${name} -s ${step} --feat afd -gamma 1.0 --uce --dist_type id -alpha 0.1 -beta 0.5 -mb 2000 -mt mean -cvd 0,1
+#exp -t ${task} -n ${name} -s ${step} --feat ard -gamma 1.0 --uce --dist_type id -alpha 0.1 -beta 0.5 -mb 2000 -mt mean -cvd 0,1
 #name=Finetune
 #exp -t ${task} -n ${name} -s ${step} -cvd 0,1
 
